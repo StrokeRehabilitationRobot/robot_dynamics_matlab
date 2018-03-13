6a8429aaa1b67b93fa65bcb66e78b6862058a489
Alexandra Valiton <33408587+Valitronix@users.noreply.github.com>
2018-03-13 13:16:55 -0400

    PD Controller tuning
    Tuning the PD controller between the end effector and the player, and the wall and the player. These values aren't totally useful, because in the game, the wall/player position difference is in pixels, and the velocity is in task space/second.

--- PDController_Test.m
+++ PDController_Test.m
@@ -2,8 +2,14 @@ clc; clear all; close all;
 
 dt = 0.01;
 
-K = 650*eye(3);
-D = 100*eye(3);
+% These parameters are for the relationship between the end effector and
+% the player
+K1 = 1850*eye(3);
+D1 = 200*eye(3);
+
+K2 = 5000*eye(3);
+D2 = 250*eye(3);
+
 A = eye(6);
 A(1:3,4:6) = dt*eye(3);
 B = zeros(6,3);
@@ -12,6 +18,10 @@ B(4:6,1:3) = dt*eye(3);
 mass = 10;
 state = [0,0,0,0,0,0]';
 desired_state = [0.3, 0.3, 0.3, 0,0,0]';
+wall_loc = [0.2,0.2,0.2]';
+%wall_loc = [2,2,2]';
+
+wall_depth = 0.05;
 
 deadline = 0.5; % seconds
 tf = 1; %seconds
@@ -20,6 +30,7 @@ trange = (0:dt:tf);
 force = zeros(length(trange), 3);
 error = zeros(length(trange), 3);
 pos = zeros(length(trange), 3);
+vel = zeros(length(trange), 3);
 % force_tip = zeros(length(trange), 3);
 % force_base = zeros(length(trange), 3);
 % pos = zeros(length(trange), 3);
@@ -27,43 +38,59 @@ for i = 1:length(trange)
     e = desired_state(1:3) - state(1:3);
     de = desired_state(4:6) - state(4:6);
     error(i,:) = e;
-    force(i,:) = K*e + D*de;
+    ee_force = K1*e + D1*de;
+    wallForce = getForceWall(K2, D2, state(1:3), state(4:6), wall_depth, wall_loc);
+    force(i,:) = ee_force - wallForce;
 %     force_base = [1, 0, 0];
 %     R = GetRotationMatrix(state(1), state(2), state(3));
 %     force_tip = R*force_base;
     xdd = force(i,:)/mass;
     state = A*state + B*xdd';
     pos(i,:) = state(1:3)';
+    vel(i,:) = state(4:6)';
 
 end
 
-
+workspace  = 1.1*desired_state(1:3);
 figure('Name', 'PD Control');
-subplot(3,1,1);
+subplot(3,2,1);
 %plot(trange, error(:,1));hold on;
 plot(trange, desired_state(1)*ones(1,length(trange))); hold on;
+plot(trange, wall_loc(1)*ones(1,length(trange)));
 plot(trange, pos(:,1));
 plot(trange, zeros(length(trange)));
-legend('des_x','pos_x'); xlabel('time');
-axis([0, tf, -0.1, 0.5]);
+legend('des_x','wall','pos_x'); xlabel('time');
+axis([0, tf, 0, workspace(1)]);
 title('X-axis');
 
-subplot(3,1,2);
+subplot(3,2,3);
 %plot(trange, error(:,2));hold on;
 %plot(trange, force(:,2));
 plot(trange, desired_state(2)*ones(1,length(trange))); hold on;
+plot(trange, wall_loc(2)*ones(1,length(trange)));
 plot(trange, pos(:,2));
 plot(trange, zeros(length(trange)));
-legend('des_y','pos_y'); xlabel('time');
-axis([0, tf, -0.1, 0.5]);
+legend('des_y','wall','pos_y'); xlabel('time');
+axis([0, tf, 0, workspace(2)]);
 title('Y-axis');
 
-subplot(3,1,3);
+subplot(3,2,5);
 %plot(trange, error(:,3));hold on;
 %plot(trange, force(:,3));
 plot(trange, desired_state(3)*ones(1,length(trange))); hold on;
+plot(trange, wall_loc(3)*ones(1,length(trange)));
 plot(trange, pos(:,3));
 plot(trange, zeros(length(trange)));
-legend('des_z','pos_z'); xlabel('time');
-axis([0, tf, -0.1, 0.5]);
+legend('des_z','wall','pos_z'); xlabel('time');
+axis([0, tf, 0, workspace(3)]);
 title('Z-axis');
+
+subplot(3,2,[2,4]);
+plot(trange, force(:,:));  hold on;
+legend;
+title('Forces');
+
+subplot(3,2,6);
+plot(trange, vel(:,:));  hold on;
+legend;
+title('Velocities');
---
+++ getForceWall.m
@@ -0,0 +1,9 @@
+function F = getForceWall(K, B, X, dX, d, W)
+dist_to_wall = abs(X-W);
+if dist_to_wall <= d
+    F = K* max(d - dist_to_wall, 0) + B*dX
+else 
+    F = 0;
+end
+
+end
